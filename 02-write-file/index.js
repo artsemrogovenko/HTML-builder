95b01698f7433d8f3bb59cff80db80a1845722e0
Artem_PC <taftoos2@gmail.com>
2025-01-16 23:46:16 +0300

    feat: add console input 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,3 +2,20 @@ const fs = require("fs");
 const path =require("path");
 const { stdin, stdout } = process;
 let writefile= path.join(__dirname,'text.txt');
+
+stdout.write("Input text:\n");
+
+
+
+stdin.on("data", (data) => {
+ let input= Buffer.from(data, "utf-8").toString();
+if(input.trim()==="exit"){
+  process.exit();
+} 
+});
+
+process.on("exit", (code) => {
+  if (code === 0) {
+    stdout.write("Good-bye!\n");
+  }
+});
