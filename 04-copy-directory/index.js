99677d7a4cbd9c711ea2ae272aacb7bee398cda8
Artem_PC <taftoos2@gmail.com>
2025-01-18 00:29:33 +0300

    feat: implement copying and updating of the catalog 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,21 +8,27 @@ const event = new EventEmitter();
 
 event.on('ready',copyDir);
 
-fs.mkdir(destFolder,{ recursive: true },(err)=>{
+fs.rm(destFolder,{recursive: true},(error)=>{
+      // console.log(error);
+    fs.mkdir(destFolder,(err)=>{
+      event.emit('ready');
+    });
+
   console.log('start copying');
-  event.emit('ready');
 });
 
 function copyDir(){
    fs.readdir(targetFolder,
   { withFileTypes: true },  (err, files) => {
- 
+
   if (err) {
-    console.log(err);
+    // console.log(err);
   } else {
     files.forEach((file) => {
         let targetPath=path.join(file.parentPath,file.name);
         let destPath=path.join(destFolder,file.name);
+          fs.copyFile(targetPath,destPath, (err) => {
+        });
     });
   }
 });
