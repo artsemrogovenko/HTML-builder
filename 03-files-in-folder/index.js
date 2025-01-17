8b2ef3ad5eac574f21d629060f9bb8680aa7d2b2
Artem_PC <taftoos2@gmail.com>
2025-01-17 18:04:20 +0300

    feat: add formated output 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,8 +9,11 @@ fs.readdir(path.join(__dirname,"secret-folder"),
   } else {
     files.forEach((file) => {
       if(file.isFile()){
-        fs.stat(path.join(file.parentPath,file.name), (err, info) => {
-          console.log(info);
+        let currentPath=path.join(file.parentPath,file.name);
+        fs.stat(currentPath, (err, info) => {
+          let extension=path.extname(currentPath);
+          let name=path.basename(currentPath,extension);
+          console.log(`${name} - ${extension.slice(1)} - ${info.size/1024}kb`);
         });
       }
     });
