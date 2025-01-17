f754cef5ae33175e317f959c49ccc6eb23e5a911
Artem_PC <taftoos2@gmail.com>
2025-01-17 17:44:48 +0300

    feat: show file stats 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const path =require("path");
+
+fs.readdir(path.join(__dirname,"secret-folder"),
+  { withFileTypes: true },  (err, files) => {
+ 
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if(file.isFile()){
+        fs.stat(path.join(file.parentPath,file.name), (err, info) => {
+          console.log(info);
+        });
+      }
+    });
+  }
+});
