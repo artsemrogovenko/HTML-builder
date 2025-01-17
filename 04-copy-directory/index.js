245b8497d34de60e4993bc53461a62681abaf982
Artem_PC <taftoos2@gmail.com>
2025-01-17 23:23:34 +0300

    feat: add initialization for copy 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 const { copyFile, constants, mkdir } = require('fs/promises');
+const { EventEmitter } = require('stream');
 const destFolder = path.join(__dirname,"files-copy");
+const targetFolder = path.join(__dirname,"files");
+const event = new EventEmitter();
+
+event.on('ready',copyDir);
+
+fs.mkdir(destFolder,{ recursive: true },(err)=>{
+  console.log('start copying');
+  event.emit('ready');
+});
+
+function copyDir(){
+   fs.readdir(targetFolder,
+  { withFileTypes: true },  (err, files) => {
+ 
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+        let targetPath=path.join(file.parentPath,file.name);
+        let destPath=path.join(destFolder,file.name);
+    });
+  }
+});
+
+}
