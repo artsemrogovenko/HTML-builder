6aa9a66ad61e06fdbe86fd2397aa3d4c209fde77
Artem_PC <taftoos2@gmail.com>
2025-01-17 15:05:41 +0300

    feat: add record to file 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,12 +3,12 @@ const path =require("path");
 const { stdin, stdout } = process;
 const { EventEmitter } = require("stream");
 let writefile= path.join(__dirname,'text.txt');
-const readableStream = fs.createReadStream(writefile);
-const writeStream =  fs.createWriteStream(writefile);
+const readableStream = fs.createReadStream(writefile,{encoding:"utf-8"});
+const writeStream =  fs.createWriteStream(writefile,{flags:"a"});
 const event = new EventEmitter();
 
-let fileData = "";
 
+readableStream.on("data", () =>{});
 readableStream.on("end", (chunk) => event.emit('ready'));
 readableStream.on("error", (error) => {
   console.log("Файл не обнаружен и создан автоматически");
@@ -25,6 +25,8 @@ event.on('ready',()=>{
    input= Buffer.from(data, "utf-8").toString();
   if(input.trim()==="exit"){
     process.exit();
+  }else{
+    writeStream.write(input);
   }
   })
 
@@ -33,6 +35,8 @@ event.on('ready',()=>{
 process.on("exit", (code) => {
   if (code === 0) {
     stdout.write("Good-bye!\n");
+    writeStream.close();
+    readableStream.close();
   }
 });
 
