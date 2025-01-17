6721f635a3e2febe43b70b94c081107bf5987bc8
Artem_PC <taftoos2@gmail.com>
2025-01-17 13:45:47 +0300

    feat: add check for file presence 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,33 @@
 const fs = require("fs");
 const path =require("path");
 const { stdin, stdout } = process;
+const { EventEmitter } = require("stream");
 let writefile= path.join(__dirname,'text.txt');
+const readableStream = fs.createReadStream(writefile);
+const writeStream =  fs.createWriteStream(writefile);
+const event = new EventEmitter();
 
-stdout.write("Input text:\n");
+let fileData = "";
+
+readableStream.on("end", (chunk) => event.emit('ready'));
+readableStream.on("error", (error) => {
+  console.log("Файл не обнаружен и создан автоматически");
+  writeStream.write("");
+  event.emit('ready');
+});
 
 
+let input="";
+event.on('ready',()=>{
+  stdout.write("Input text:\n");
 
-stdin.on("data", (data) => {
- let input= Buffer.from(data, "utf-8").toString();
-if(input.trim()==="exit"){
-  process.exit();
-} 
+  stdin.on("data", (data) => {
+   input= Buffer.from(data, "utf-8").toString();
+  if(input.trim()==="exit"){
+    process.exit();
+  }
+  })
+
 });
 
 process.on("exit", (code) => {
