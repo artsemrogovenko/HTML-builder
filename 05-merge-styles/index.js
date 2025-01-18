3f964f31d0867f0a61386ddd5812aa037fd74188
Artem_PC <taftoos2@gmail.com>
2025-01-18 22:25:24 +0300

    feat: add reading only css files 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,3 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const { EventEmitter } = require('stream');
 const destFolder = path.join(__dirname,"project-dist");
+const targetFolder = path.join(__dirname,"styles");
+
+fs.readdir(targetFolder,
+  { withFileTypes: true },  (err, files) => {
+ 
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if(file.isFile()){
+        let currentPath=path.join(file.parentPath,file.name);
+        fs.stat(currentPath, (err, info) => {
+          let extension=path.extname(currentPath);
+          if(extension===".css"){
+          }
+        });
+      }
+    });
+  }
+});
