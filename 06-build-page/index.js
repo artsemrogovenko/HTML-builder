d9488cd2c49d86167d19b1cd4e9936fbba40baa0
Artem_PC <taftoos2@gmail.com>
2025-01-19 17:02:00 +0300

    feat: create initialization for build, prepare functions for work task 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,3 +6,39 @@ const destFolder = path.join(__dirname,"project-dist");
 const cssFolder = path.join(__dirname,"styles");
 const assetsFolder = path.join(__dirname,"assets");
 const buildedHtml = path.join(destFolder,"index.html");
+
+const event = new EventEmitter();
+event.on('ready',copyHtml);
+
+(()=>{
+  fs.rm(destFolder, { recursive: true }, (error) => {
+      fs.mkdir(destFolder, (e) => {
+        event.emit('ready');
+      });
+    });
+    console.log('start build page');
+})()
+
+function copyHtml(){
+  fs.copyFile(path.join(__dirname,'template.html'), buildedHtml, (err) => {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+}
+
+function readFile(path){
+}
+
+function mergeStyles(){
+}
+
+function replaceSection(htmlPath){
+}
+
+function cloneFolders(){
+}
+
+function startBuild(){
+
+}
