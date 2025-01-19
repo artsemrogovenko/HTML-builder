847a0476356e9add2dc693fc34e7ac3dba82cef3
Artem_PC <taftoos2@gmail.com>
2025-01-19 12:51:14 +0300

    feat: add write into bundle.css 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,22 +3,34 @@ const path = require('path');
 const { EventEmitter } = require('stream');
 const destFolder = path.join(__dirname,"project-dist");
 const targetFolder = path.join(__dirname,"styles");
+const fileName = path.join(destFolder,"bundle.css");
 
-fs.readdir(targetFolder,
+fs.unlink(fileName,write);
+
+function write(){
+  fs.readdir(targetFolder,
   { withFileTypes: true },  (err, files) => {
  
   if (err) {
     console.log(err);
   } else {
+    const writeStream =  fs.createWriteStream(fileName,{flags:"a"});
+
     files.forEach((file) => {
       if(file.isFile()){
         let currentPath=path.join(file.parentPath,file.name);
         fs.stat(currentPath, (err, info) => {
           let extension=path.extname(currentPath);
           if(extension===".css"){
+            let readfile= currentPath;
+            const stream = fs.createReadStream(readfile,'utf-8');
+            stream.on("data", (data) => {
+              writeStream.write(data);
+            });
           }
         });
       }
     });
   }
 });
+}
